7e86a05328681de73022e1973b4d77727002d79d
FLSoz <65517812+FLSoz@users.noreply.github.com>
2024-12-02 22:36:51 -0800

    Update puzzle3b.py

--- puzzle3b.py
+++ puzzle3b.py
@@ -2,11 +2,16 @@ with open("puzzle3_input.txt", "r") as f:
 	lines = f.readlines()
 
 import re
-p = re.compile('mul\(([0-9]+),([0-9]+)\)')
+p = re.compile("mul\(([0-9]+),([0-9]+)\)|(do\(\))|(don't\(\))")
 acc = 0
+record = True
 for line in lines:
 	matches = p.findall(line)
 	for match in matches:
-		acc += int(match[0]) * int(match[1])
-
+		if match[2] == "do()":
+			record = True
+		elif match[3] == "don't()":
+			record = False
+		elif record:
+			acc += int(match[0]) * int(match[1])
 print(acc)
